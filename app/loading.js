bf34186aa74dfdea5cc0c9cbb27086fa481b2ba0
asthapatel-collab <astha@collabnow.us>
2025-01-22 14:58:09 -0500

    Added few pages and styles, database initiates

---
+++ app/loading.js
@@ -0,0 +1,5 @@
+import classed from './loading.module.css';
+
+export default function Loading() {
+    return <p className={classed.loading}>Fetching Meals...</p>
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,27 @@
-export default function MealsPage(){
-    return <h1> Meals</h1>
+import Link from "next/link";
+import classes from "./page.module.css";
+import { MealGrid } from "@/component/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favroite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favroite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealGrid meals={meals} />
+      </main>
+    </>
+  );
 }
